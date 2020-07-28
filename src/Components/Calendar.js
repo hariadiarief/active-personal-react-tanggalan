0adc5cf8d2c26b5f942dfa2524f473467a3975a4
hariadiarief <adi.arief77@gmail.com>
2020-07-28 23:56:57 +0700

    rewording

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -10,27 +10,20 @@ export default class Calendar extends Component {
 		this.state = {
 			weeksDisplay: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fa', 'Sa'],
 			today: moment(),
-			currentDate: moment(),
-			selectionDate: null,
+			whichMonthDisplay: moment(),
+			selectedDate: null,
 		}
 	}
 
 	determineDateClass(date) {
-		const { currentDate, today, selectionDate } = this.state
-		const { defaultDate = null } = this.props
-		if (
-			(selectionDate && date.format('YYYY-MM-DD') === selectionDate.format('YYYY-MM-DD')) ||
-			defaultDate === date.format('YYYY-MM-DD')
-		) {
-			// if (date.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
-			// 	return 'calendar-dates--today-selected'
-			// }
+		const { whichMonthDisplay, today, selectedDate } = this.state
+		if (selectedDate && date.format('YYYY-MM-DD') === selectedDate.format('YYYY-MM-DD')) {
 			return 'calendar-dates--selected'
 		}
 		if (date.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
 			return 'calendar-dates--today'
 		}
-		if (date.format('YYYY-MM') === currentDate.format('YYYY-MM')) {
+		if (date.format('YYYY-MM') === whichMonthDisplay.format('YYYY-MM')) {
 			return 'calendar-dates--focus'
 		} else {
 			return 'calendar-dates--infocus'
@@ -38,17 +31,21 @@ export default class Calendar extends Component {
 	}
 
 	render() {
-		const { currentDate, today, selectionDate, weeksDisplay } = this.state
+		const { whichMonthDisplay, today, selectedDate, weeksDisplay } = this.state
 		const { onChange = () => null } = this.props
 
 		return (
 			<div className='calendar'>
 				<div className='calendar-month'>
-					<button type='button' onClick={() => this.setState({ currentDate: currentDate.subtract(1, 'month') })}>
+					<button
+						type='button'
+						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.subtract(1, 'month') })}>
 						<IconArrowLeft />
 					</button>
-					<div>{currentDate.format('MMMM YYYY')}</div>
-					<button type='button' onClick={() => this.setState({ currentDate: currentDate.add(1, 'month') })}>
+					<div>{whichMonthDisplay.format('MMMM YYYY')}</div>
+					<button
+						type='button'
+						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.add(1, 'month') })}>
 						<IconArrowRight />
 					</button>
 				</div>
@@ -61,13 +58,13 @@ export default class Calendar extends Component {
 
 				<div className='calendar-dates'>
 					{Array.apply(null, Array(35)).map((_, index) => {
-						const date = currentDate.clone().startOf('month').startOf('week').add(index, 'day')
+						const date = whichMonthDisplay.clone().startOf('month').startOf('week').add(index, 'day')
 						return (
 							<button type='button' key={index}>
 								<span
 									className={this.determineDateClass(date)}
 									onClick={() =>
-										this.setState({ selectionDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
+										this.setState({ selectedDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
 									}>
 									{date.format('D')}
 								</span>
