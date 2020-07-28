0fc73459acca7860c8c0f37663fc3430c196d2ec
hariadiarief <adi.arief77@gmail.com>
2020-07-28 23:44:19 +0700

    finishing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Calendar from './Components/Calendar'
 
 function App() {
 	return (
-		<div className='continer'>
-			<div className='card'>
+		<div className='container home'>
+			<h1>Responsive Calendar Component</h1>
+			<div></div>
+			<div className='card home__card'>
 				<Calendar onChange={(date) => alert(date)} />
 			</div>
 		</div>
--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 
+import { ReactComponent as IconArrowLeft } from '../Assets/Icons/keyboard_arrow_left.svg'
+import { ReactComponent as IconArrowRight } from '../Assets/Icons/keyboard_arrow_right.svg'
+
 export default class Calendar extends Component {
 	constructor(props) {
 		super(props)
@@ -19,6 +22,9 @@ export default class Calendar extends Component {
 			(selectionDate && date.format('YYYY-MM-DD') === selectionDate.format('YYYY-MM-DD')) ||
 			defaultDate === date.format('YYYY-MM-DD')
 		) {
+			// if (date.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
+			// 	return 'calendar-dates--today-selected'
+			// }
 			return 'calendar-dates--selected'
 		}
 		if (date.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
@@ -32,23 +38,34 @@ export default class Calendar extends Component {
 	}
 
 	render() {
-		const { currentDate, today } = this.state
+		const { currentDate, today, selectionDate } = this.state
 		const { onChange = () => null } = this.props
 
 		return (
 			<div className='calendar'>
+				<div className='calendar-month'>
+					<button type='button' onClick={() => this.setState({ currentDate: currentDate.subtract(1, 'month') })}>
+						<IconArrowLeft />
+					</button>
+					<div>{currentDate.format('MMMM YYYY')}</div>
+					<button type='button' onClick={() => this.setState({ currentDate: currentDate.add(1, 'month') })}>
+						<IconArrowRight />
+					</button>
+				</div>
+
 				<div className='calendar-dates'>
 					{Array.apply(null, Array(35)).map((_, index) => {
 						const date = currentDate.clone().startOf('month').startOf('week').add(index, 'day')
 						return (
-							<div
-								key={index}
-								className={this.determineDateClass(date)}
-								onClick={() =>
-									this.setState({ selectionDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
-								}>
-								{date.format('D')}
-							</div>
+							<button type='button' key={index}>
+								<span
+									className={this.determineDateClass(date)}
+									onClick={() =>
+										this.setState({ selectionDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
+									}>
+									{date.format('D')}
+								</span>
+							</button>
 						)
 					})}
 				</div>
