abdf7e6fb9c88acabe14961a3e8eb872cfb22716
Masagus Hariadi Arief <adi.arief77@gmail.com>
2020-10-02 23:59:43 +0700

    update calendar component with hook

---
+++ src/Components/Calendar-Legacy.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import moment from 'moment'
+
+import { ReactComponent as IconArrowLeft } from '../Assets/Icons/keyboard_arrow_left.svg'
+import { ReactComponent as IconArrowRight } from '../Assets/Icons/keyboard_arrow_right.svg'
+
+export default class Calendar extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			weeksDisplay: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fa', 'Sa'],
+			today: moment(),
+			whichMonthDisplay: this.props.selectedDate ? moment(this.props.selectedDate) : moment(),
+			selectedDate: moment(this.props.selectedDate) || moment(),
+		}
+	}
+
+	determineDateClass(date) {
+		const { whichMonthDisplay, today, selectedDate } = this.state
+		if (selectedDate && date.format('YYYY-MM-DD') === selectedDate.format('YYYY-MM-DD')) {
+			return 'calendar-dates--selected'
+		}
+		if (date.format('YYYY-MM-DD') === today.format('YYYY-MM-DD')) {
+			return 'calendar-dates--today'
+		}
+		if (date.format('YYYY-MM') === whichMonthDisplay.format('YYYY-MM')) {
+			return 'calendar-dates--focus'
+		} else {
+			return 'calendar-dates--infocus'
+		}
+	}
+
+	render() {
+		const { whichMonthDisplay, weeksDisplay } = this.state
+		const { onChange = () => null } = this.props
+
+		return (
+			<div className='calendar'>
+				<div className='calendar-month'>
+					<button
+						type='button'
+						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.subtract(1, 'month') })}>
+						<IconArrowLeft />
+					</button>
+					<div>{whichMonthDisplay.format('MMMM YYYY')}</div>
+					<button
+						type='button'
+						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.add(1, 'month') })}>
+						<IconArrowRight />
+					</button>
+				</div>
+
+				<div className='calendar-weeks'>
+					{weeksDisplay.map((week) => (
+						<div>{week}</div>
+					))}
+				</div>
+
+				<div className='calendar-dates'>
+					{Array.apply(null, Array(35)).map((_, index) => {
+						const date = whichMonthDisplay.clone().startOf('month').startOf('week').add(index, 'day')
+						return (
+							<button type='button' key={index}>
+								<span
+									className={this.determineDateClass(date)}
+									onClick={() =>
+										this.setState({ selectedDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
+									}>
+									{date.format('D')}
+								</span>
+							</button>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
+}
--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 
 import { ReactComponent as IconArrowLeft } from '../Assets/Icons/keyboard_arrow_left.svg'
 import { ReactComponent as IconArrowRight } from '../Assets/Icons/keyboard_arrow_right.svg'
 
-export default class Calendar extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			weeksDisplay: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fa', 'Sa'],
-			today: moment(),
-			whichMonthDisplay: this.props.selectedDate ? moment(this.props.selectedDate) : moment(),
-			selectedDate: moment(this.props.selectedDate) || moment(),
-		}
-	}
+export default function Calendar(props) {
+	const weeksDisplay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fa', 'Sa']
+	const today = moment()
 
-	determineDateClass(date) {
-		const { whichMonthDisplay, today, selectedDate } = this.state
+	const [selectedDate, setSelectedDate] = useState(moment(props.selectedDate) ?? moment())
+	const [whichMonthDisplay, setWhichMonthDisplay] = useState(
+		props.selectedDate ? moment(props.selectedDate) : moment()
+	)
+
+	const determineDateClass = (date) => {
 		if (selectedDate && date.format('YYYY-MM-DD') === selectedDate.format('YYYY-MM-DD')) {
 			return 'calendar-dates--selected'
 		}
@@ -30,49 +27,41 @@ export default class Calendar extends Component {
 		}
 	}
 
-	render() {
-		const { whichMonthDisplay, weeksDisplay } = this.state
-		const { onChange = () => null } = this.props
-
-		return (
-			<div className='calendar'>
-				<div className='calendar-month'>
-					<button
-						type='button'
-						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.subtract(1, 'month') })}>
-						<IconArrowLeft />
-					</button>
-					<div>{whichMonthDisplay.format('MMMM YYYY')}</div>
-					<button
-						type='button'
-						onClick={() => this.setState({ whichMonthDisplay: whichMonthDisplay.add(1, 'month') })}>
-						<IconArrowRight />
-					</button>
-				</div>
-
-				<div className='calendar-weeks'>
-					{weeksDisplay.map((week) => (
-						<div>{week}</div>
-					))}
-				</div>
-
-				<div className='calendar-dates'>
-					{Array.apply(null, Array(35)).map((_, index) => {
-						const date = whichMonthDisplay.clone().startOf('month').startOf('week').add(index, 'day')
-						return (
-							<button type='button' key={index}>
-								<span
-									className={this.determineDateClass(date)}
-									onClick={() =>
-										this.setState({ selectedDate: date.clone() }, () => onChange(date.format('YYYY-MM-DD')))
-									}>
-									{date.format('D')}
-								</span>
-							</button>
-						)
-					})}
-				</div>
+	return (
+		<div className='calendar'>
+			<div className='calendar-month'>
+				<button type='button' onClick={() => setWhichMonthDisplay(whichMonthDisplay.clone().subtract(1, 'month'))}>
+					<IconArrowLeft />
+				</button>
+				<div>{whichMonthDisplay.format('MMMM YYYY')}</div>
+				<button type='button' onClick={() => setWhichMonthDisplay(whichMonthDisplay.clone().add(1, 'month'))}>
+					<IconArrowRight />
+				</button>
 			</div>
-		)
-	}
+
+			<div className='calendar-weeks'>
+				{weeksDisplay.map((week) => (
+					<div>{week}</div>
+				))}
+			</div>
+
+			<div className='calendar-dates'>
+				{Array.apply(null, Array(35)).map((_, index) => {
+					const date = whichMonthDisplay.clone().startOf('month').startOf('week').add(index, 'day')
+					return (
+						<button type='button' key={index}>
+							<span
+								className={determineDateClass(date)}
+								onClick={() => {
+									setSelectedDate(date.clone())
+									props.onChange(date.format('YYYY-MM-DD'))
+								}}>
+								{date.format('D')}
+							</span>
+						</button>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
