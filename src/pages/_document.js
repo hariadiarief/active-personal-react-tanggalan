a0c68d1f05a75498533dc7ef3b810038632189a1
Masagus Hariadi Arief <adi.arief77@gmail.com>
2020-11-09 19:50:10 +0700

    cp

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,12 +19,13 @@ export default class extends Document {
 					<meta name='description' content={APP_DESCRIPTION} />
 					<meta name='format-detection' content='telephone=no' />
 					<meta name='mobile-web-app-capable' content='yes' />
-					<meta name='theme-color' content='#FFFFFF' />
+					<meta name='theme-color' content='#00bcff' />
 					{/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
 					{/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
 
 					<link rel='apple-touch-icon' sizes='180x180' href='/icons/calendar.png' />
-					<link rel='manifest' href='/manifest.json' />
+					{/* <link rel='manifest' href='/manifest.json' /> */}
+					<link rel='manifest' href='dunplab-manifest-28409.json' />
 					<link rel='shortcut icon' href='/icons/favicon.ico' />
 					<style>{`
             html, body, #__next {
