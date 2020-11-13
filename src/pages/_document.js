80a92225342217c871f5675b9bcc57930d6b9e0d
Masagus Hariadi Arief <adi.arief77@gmail.com>
2020-11-13 22:28:29 +0700

    update manifest

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,7 +19,7 @@ export default class extends Document {
 					<meta name='description' content={APP_DESCRIPTION} />
 					<meta name='format-detection' content='telephone=no' />
 					<meta name='mobile-web-app-capable' content='yes' />
-					<meta name='theme-color' content='#00bcff' />
+					<meta name='theme-color' content='#828282' />
 					{/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
 					{/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
 
